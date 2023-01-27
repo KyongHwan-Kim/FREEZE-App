85ca00696105c7a654001078378e878aca03f10a
kyounghwan kim <dolkys123@sju.ac.kr>
2023-01-27 21:14:45 +0900

    add color convert utility

---
+++ freeze/lib/utilities/utility.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+Color fromHex(String hexString) {
+  hexString = hexString.replaceAll("#", "");
+  if (hexString.length == 6) {
+    hexString = "FF" + hexString;
+  } else if (hexString.length == 8) {
+    return Color(int.parse("0x$hexString"));
+  }
+}
